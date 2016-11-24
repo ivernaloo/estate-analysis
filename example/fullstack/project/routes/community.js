2e604b21257a7330fd7c1ec9a92a697372acb7dc
aloo <aloocs@gmail.com>
2016-11-24 18:50:52 +0800

    加入了数据加限制，加速加载

--- example/fullstack/project/routes/community.js
+++ example/fullstack/project/routes/community.js
@@ -13,7 +13,7 @@ router.get('/', function(req, res, next) {
 	}, function(e, ds) {
 		console.log(ds.length)
 		res.json(ds);
-	}).limit(100);  // 加入了长度限制，加速数据加载
+	}).limit(1000);  // 加入了长度限制，加速数据加载
 });
 
 module.exports = router;
